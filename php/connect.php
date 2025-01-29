c4c18e6f1eb3db8c85d1407440adfa812ec9a17a
zapatamrc <zapata2235396@mls.ceu.edu.ph>
2025-01-29 14:57:08 +0800

    backend added

--- php/connect.php
+++ php/connect.php
@@ -1,31 +1,28 @@
-<?php 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-    $conn = new mysqli("localhost", "root", "", "ict");
+$conn = new mysqli("192.168.254.108", "root", "", "ict");
 
-    if ($conn->connect_error) {
-        die('Failed to connect: ' . $conn->connect_error);
-    } else {
-        $stmt = $conn->prepare("SELECT * FROM ictuser WHERE username=?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $stmt_result = $stmt->get_result();
+if ($conn->connect_error) {
+    die('Failed to connect: ' . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("SELECT * FROM ictuser WHERE username=?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt_result = $stmt->get_result();
 
-        if ($stmt_result->num_rows > 0) {
-            $data = $stmt_result->fetch_assoc();
+    if ($stmt_result->num_rows > 0) {
+        $data = $stmt_result->fetch_assoc();
 
-            if (password_verify($password, $data['password'])) {
-                echo "<h2>Login Successful</h2>";
-            } else {
-                echo "<h2>Invalid Username or Password</h2>";
-            }
+        if ($data['password'] === $password) {
+            header("Location: /UR1/PrevMntnc/upd/index.html");
+            exit;
         } else {
             echo "<h2>Invalid Username or Password</h2>";
         }
-
-        // Close statement and connection
-        $stmt->close();
-        $conn->close();
+    } else {
+        echo "<h2>Invalid Username or Password</h2>";
     }
+}
 ?>
